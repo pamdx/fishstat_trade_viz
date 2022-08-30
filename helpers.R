0369a33c52730c0103a0e9687a510e78552f59fe
Pierre Maudoux <pierre.ami.maudoux@gmail.com>
2022-08-30 15:06:17 +0200

    Fixed missing coordinates data

--- helpers.R
+++ helpers.R
@@ -8,6 +8,16 @@ options(highcharter.theme = newtheme)
 
 # Load data
 
+cou_coordinates <- read_csv("https://raw.githubusercontent.com/google/dspl/master/samples/google/canonical/countries.csv", na = "") %>%
+  rename(ISO2 = country) %>%
+  select(ISO2, latitude, longitude) %>%
+  add_row(ISO2 = "SU", latitude = 61.524010, longitude = 105.318756) %>% # Add USSR using Russia's coordinates
+  add_row(ISO2 = "BQ", latitude = 12.201890, longitude = -68.262383) %>% # Add Bonaire's coordinates
+  add_row(ISO2 = "CW", latitude = 12.169570, longitude = -68.990021) %>% # Add Curaçao's coordinates
+  add_row(ISO2 = "CS", latitude = 44.016521, longitude = 21.005859) %>% # Add Serbia and Montenegro using Serbia's coordinates
+  add_row(ISO2 = "SS", latitude = 4.859363, longitude = 31.571251) %>% # Add South Sudan's coordinates
+  add_row(ISO2 = "YU", latitude = 44.016521, longitude = 21.005859) # Add Yugoslavia using Serbia's coordinates
+
 edata <- readRDS("trade_partner_agg.RDS")
 
 map <- readRDS("map.RDS")
